a3752fb95c388efc0520e53856ddc2aa87edc7f9
ruotingw <wruoting@gmail.com>
2020-05-31 13:37:04 -0400

    Updated with comments

--- assignment_2_wang.py
+++ assignment_2_wang.py
@@ -114,14 +114,23 @@ def main():
     for year in years:
         df_q2 = df_q2.append(create_daily_returns(df, year))
     print(df_q2)
-    print('The average daily return between years varies drastically, PUT EXPLANATION HERE')
+    print('The average daily return between years varies from -0.12 % to 0.14 %. While the distribution of returns is generally assumed to be not normal, in 2016 the percentage of days with gains more than')
+    print('mu and less than the mu were the same. Deviations from 50%% greater and less than mu were not larger than 2.78%. This does not indicate that the distribution is normal, but it would indicate ')
+    print('that there is no incredibly high right or left skew. Interestingly, the years with below 0 daily average returns had a higher percentage of days trading greater than the mean. All other years have a greater percentage ')
+    print('of days trading with returns below the mean. This implies that most years will have more negative trading days than positive trading days, which would follow a pattern of a mild right skew distribution.')
 
-    print('Question 3: ')
+    print('Question 3 and Question 4: ')
     df_q3 = pd.DataFrame(columns=['Year', 'Trading Days', 'mu', 'sigma', '%% days < mu - 2 * sigma', '%% days > mu + 2 * sigma'])
     # Append each year to our dataframe
     for year in years:
         df_q3 = df_q3.append(create_daily_returns_with_std_deviation(df, year))
     print(df_q3)
+    print('We expect that given a 2 tail standard deviation calculation, 2 standard deviations from the mean would yield about 2.5% per tail. The calculation shown above ')
+    print('indicates that this is not the case. Each year has a significantly different percentages in its tails, usually with more in its left tail than its right tail. ')
+    print('This suggests that there are significantly more "really bad trading days" than "really good trading days" indicating that bigger drops occur more frequently than ')
+    print('bigger gains. On years with a high standard deviation of daily returns (eg. more volatile), namely 2015, 2016, and 2018, there is a significantly higher percentage of bad trading days ')
+    print('compared to good ones: 3.17% vs 1.98%, 3.57% vs 0.79%, and 1.99% vs 0.8%. On years with high positive average returns, namely 2017, there is a higher percentage of "really good trading days" than "really bad trading days", but not by as much ')
+    print('as the difference in 2015, 2016, and 2018. It seems as though generally, there are a higher percentage of big drops than big gains overall, and this is exacerbated by increased volatility (sigma) which deviates with majorly bullish years.')
 
 if __name__ == "__main__":
     main()
